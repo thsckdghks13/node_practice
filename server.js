9a4ed267b021d7354bbdeb0e212b2607458c7c8f
쿠로쿠치 <thsckdghks13@naver.com>
2022-09-23 10:12:57 +0900

    index없는 모든걸 검색하는 검색 로직

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ app.use(bodyParser.urlencoded({extended : true}));
 const MongoClient = require('mongodb').MongoClient;
 const methodOverride = require('method-override')
 app.use(methodOverride('_method'))
+require('dotenv').config()
+
 app.set('view engine', 'ejs');
 
 app.use('/public', express.static('public'));
@@ -20,12 +22,12 @@ app.get('/write', function(req, res){
 
 
 var db;
-MongoClient.connect('mongodb+srv://gto159:asd123@cluster0.tkjf8is.mongodb.net/?retryWrites=true&w=majority', function(error, client){
+MongoClient.connect(process.env.DB_URL, function(error, client){
      if(error) return console.log(error)
 
     db = client.db('todoapp')  //todoapp 데이터베이스 연결
 
-    app.listen(8080, function(){
+    app.listen(process.env.PORT, function(){
         console.log('listening on 8080')
     });
 
@@ -67,6 +69,15 @@ MongoClient.connect('mongodb+srv://gto159:asd123@cluster0.tkjf8is.mongodb.net/?r
 
     });
 
+
+    app.get('/search', function(req, res){
+        db.collection('post').find({ 제목 : req.query.value}).toArray(function(error, result){
+            console.log(result)
+            res.render('search.ejs', { searchD : result });
+        });
+    });
+
+    
     app.delete('/delete', function(req, res){
         req.body._id = parseInt(req.body._id);
         var deletebody = req.body
@@ -109,11 +120,72 @@ MongoClient.connect('mongodb+srv://gto159:asd123@cluster0.tkjf8is.mongodb.net/?r
     })
 });
 
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+
+app.use(session({secret : '비밀코드', resave : true, saveUninitialized : false}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/login',function(req, res){
+    res.render('login.ejs')
+});
+
+app.post('/login', passport.authenticate('local',{
+    failureRedirect : '/fail'
+}), function(req, res){
+    res.redirect('/')
+});
+
+app.get('/mypage', login, function(req, res){
+    console.log(req.user);
+    res.render('mypage.ejs', {user : req.user});
+});
+
+function login(req, res, next){ //미들웨어
+    if(req.user){
+        next()
+    } else {
+        res.send('로그인 안함')
+    }
+}
+
+passport.use(new LocalStrategy({
+    usernameField: 'id',
+    passwordField: 'pw',
+    session: true,
+    passReqToCallback: false,
+  }, function (입력한아이디, 입력한비번, done) {
+    //console.log(입력한아이디, 입력한비번);
+    db.collection('login').findOne({ id: 입력한아이디 }, function (error, result) {
+      if (error) return done(error)
+  
+      if (!result) return done(null, false, { message: '존재하지않는 아이디요' })
+      if (입력한비번 == result.pw) {
+        return done(null, result)
+      } else {
+        return done(null, false, { message: '비번틀렸어요' })
+      }
+    })
+  }));
 
 
-/*
+passport.serializeUser(function(user, done){
+    done(null, user.id)
+});
 
-*/
+passport.deserializeUser(function(아이디, done){
+    //DB에서 위에있는 user.id로 유저를 찾은 후 유저 정보를 뱉어냄
+    db.collection('login').findOne({id : 아이디}, function(error, result){
+        done(null, {result})
+    })
+
+});
+
+
+
+
 
 // req = request '요청'
 // res = response '응답'
